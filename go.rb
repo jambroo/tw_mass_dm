60a039373ad89a9bc4854775e1073c65cc55ea90
James Brooking <jambroo@gmail.com>
2017-06-05 00:13:40 +0100

    rate limit errors.

--- go.rb
+++ go.rb
@@ -7,8 +7,6 @@ tw_secret = ENV["TW_SECRET"]
 tw_username = ENV["TW_USERNAME"]
 
 authorized = `twurl accounts`
-puts
-puts ".."
 if authorized == ""
   auth_result = system("twurl authorize --consumer-key %s --consumer-secret %s" % [tw_key, tw_secret])
   if !auth_result
@@ -30,16 +28,19 @@ def get_followers(cursor)
 
   File.open("/tmp/result", 'w') { |file| file.write(followers_result) }
 
+  if `cat /tmp/result | jq '.errors | length'`.to_i > 0
+    abort("ERROR: rate limit reached")
+  end
+
   screen_names = `cat /tmp/result | jq -r '.users[].screen_name'`.split("\n")
   next_cursor = `cat /tmp/result | jq '.next_cursor'`
 
   File.open("/tmp/followers.cache", 'a') { |file| file.write(screen_names.join("\n")) }
 
-  # confirm this condition works
-  if next_cursor != 0
+  if next_cursor.to_i != 0
     get_followers(next_cursor)
   end
 end
 
-get_followers(1534795388399221800)
-#get_followers(-1)
+get_followers(-1)
+
