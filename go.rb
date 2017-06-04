5380554526e28a40e571f29b4d1979f3ce151ae0
James Brooking <jambroo@gmail.com>
2017-06-04 22:28:40 +0100

    progress.

--- go.rb
+++ go.rb
@@ -7,7 +7,9 @@ tw_secret = ENV["TW_SECRET"]
 tw_username = ENV["TW_USERNAME"]
 
 authorized = `twurl accounts`
-if !authorized
+puts
+puts ".."
+if authorized == ""
   auth_result = system("twurl authorize --consumer-key %s --consumer-secret %s" % [tw_key, tw_secret])
   if !auth_result
     abort("ERROR: Auth handshake failed.")
@@ -19,16 +21,25 @@ if !username_result
   abort("ERROR: Supplied username not found.")
 end
 
-#followers
-followers_result = `twurl /1.1/friends/list.json?cursor=1552369174870030800`
-if !followers_result
-  abort("ERROR: could not get friends list")
+def get_followers(cursor)
+  #followers
+  followers_result = `twurl /1.1/friends/list.json?cursor=#{cursor}`
+  if !followers_result
+    abort("ERROR: could not get friends list")
+  end
+
+  File.open("/tmp/result", 'w') { |file| file.write(followers_result) }
+
+  screen_names = `cat /tmp/result | jq -r '.users[].screen_name'`.split("\n")
+  next_cursor = `cat /tmp/result | jq '.next_cursor'`
+
+  File.open("/tmp/followers.cache", 'a') { |file| file.write(screen_names.join("\n")) }
+
+  # confirm this condition works
+  if next_cursor != 0
+    get_followers(next_cursor)
+  end
 end
 
-File.open("/tmp/no0", 'w') { |file| file.write(followers_result) }
-
-screen_names = `cat /tmp/no0 | jq -r '.users[].screen_name'`.split("\n")
-next_cursor = `cat /tmp/no0 | jq '.next_cursor'`
-
-puts screen_names
-puts next_cursor
+get_followers(1534795388399221800)
+#get_followers(-1)
