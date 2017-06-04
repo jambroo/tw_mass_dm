8ee36247055b9b41ac567a69c6d998a64a482256
James Brooking <jambroo@gmail.com>
2017-06-04 20:05:25 +0100

    very username is associated with account.

--- go.rb
+++ go.rb
@@ -4,8 +4,14 @@ end
 
 tw_key = ENV["TW_KEY"]
 tw_secret = ENV["TW_SECRET"]
+tw_username = ENV["TW_USERNAME"]
 
 result = system("twurl authorize --consumer-key %s --consumer-secret %s" % [tw_key, tw_secret])
 if !result
   abort("ERROR: Auth handshake failed.")
+end
+
+result = system("twurl set default %s" % [tw_username])
+if !result
+  abort("ERROR: Supplied username not found.")
 end
